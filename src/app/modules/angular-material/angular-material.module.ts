3a210aa38c97b699d829226744de1755c3afc1dc
laska-adief <laskaadief.la@gmail.com>
2023-05-09 06:58:28 +0700

    [Backoffice Employee] add angular material modules

---
+++ src/app/modules/angular-material/angular-material.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatSelectModule } from '@angular/material/select';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
+const modules = [
+  MatFormFieldModule,
+  MatInputModule,
+  MatButtonModule,
+  MatTableModule,
+  MatIconModule,
+  MatPaginatorModule,
+  MatSortModule,
+  MatProgressSpinnerModule,
+  MatSnackBarModule,
+  MatDatepickerModule,
+  MatAutocompleteModule,
+  MatSelectModule,
+  MatMenuModule,
+  MatTooltipModule,
+];
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, ...modules],
+  exports: [...modules],
+})
+export class AngularMaterialModule {}
