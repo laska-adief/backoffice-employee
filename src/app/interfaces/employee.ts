5bfeadbbfcb033f576029acf07d07a3398872512
laska-adief <laskaadief.la@gmail.com>
2023-05-08 21:01:33 +0700

    [Backoffice Employee] employee interface

---
+++ src/app/interfaces/employee.ts
@@ -0,0 +1,12 @@
+export interface Employee {
+  id: string;
+  username: string;
+  firstname: string;
+  lastname: string;
+  email: string;
+  birthDate: Date;
+  basicSalary: string;
+  status: string;
+  group: string;
+  description: string;
+}
