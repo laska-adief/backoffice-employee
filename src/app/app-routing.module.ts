2799a07ce7a1a7164a36add8fdab958602443651
laska-adief <laskaadief.la@gmail.com>
2023-05-09 06:57:59 +0700

    [Backoffice Employee] add authguard canActivate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'employee', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   {
     path: 'employee',
+    canActivate: [AuthGuard],
     loadChildren: () =>
       import('./pages/employee/employee.module').then((m) => m.EmployeeModule),
   },
