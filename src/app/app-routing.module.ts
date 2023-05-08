c704e4f4ced0d82e823e2aab251c8643105bd6f5
laska-adief <laskaadief.la@gmail.com>
2023-05-08 20:58:56 +0700

    [Backoffice Employee] Routing components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'employee', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'employee',
+    loadChildren: () =>
+      import('./pages/employee/employee.module').then((m) => m.EmployeeModule),
+  },
+  { path: '**', redirectTo: 'employee' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
