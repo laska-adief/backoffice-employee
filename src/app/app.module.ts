1e7effc6c400c27ee719721fa62413cbb2eb8721
laska-adief <laskaadief.la@gmail.com>
2023-05-08 20:59:11 +0700

    [Backoffice Employee] add modules and declaration components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material/core';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { EmployeeListComponent } from './pages/employee/employee-list/employee-list.component';
+import { EmployeeFormComponent } from './pages/employee/employee-form/employee-form.component';
+import { EmployeeDetailComponent } from './pages/employee/employee-detail/employee-detail.component';
+import { EmployeeComponent } from './pages/employee/employee.component';
+import { AngularMaterialModule } from './modules/angular-material/angular-material.module';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    EmployeeListComponent,
+    EmployeeFormComponent,
+    EmployeeDetailComponent,
+    EmployeeComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AngularMaterialModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatNativeDateModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
